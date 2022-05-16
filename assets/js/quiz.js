cf0e6ea8a8c2198b59c9d80cd852c2658c4c8400
unkhan87 <ukhan87@icloud.com>
2022-05-16 09:14:59 +0000

    display hard coded questions and options

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,3 @@
-
 const question = document.getElementById("league-questions");
 /*To convert the options to array*/
 const options = Array.from(document.getElementsByClassName("option-text"));
@@ -12,86 +11,83 @@ let questionCounter = 0;
 let remainingQuestions = [];
 let currentIndex = 0;
 
-let questions = [
-    {
-       question: "What team won the first ever Premier League season?",
-       option1: "Manchester United",
-       option2: "Aston Villa",
-       option3: "Liverpool",
-       option4: "Queens Park Ranger",
-       answer: 1,
-    },
-    {
-       question: "Name the Champions of the 2020/21 season?",
-       option1: "Manchester United",
-       option2: "Chelsea",
-       option3: "Manchester City",
-       option4: "Manchester United",
-       answer: 3,
-    },
-    {
-        question: "Which team went an entire season without losing a game in 2003/04?",
-        option1: "Manchester United",
-        option2: "Arsenal",
-        option3: "Liverpool",
-        option4: "Chelsea",
-        answer: 2,
-     },
-     {
-        question: "Which team won their first ever Premier League title in 2016?",
-        option1: "Tottenham Hotspur",
-        option2: "Leicester City",
-        option3: "Everton",
-        option4: "Southampton F.C",
-        answer: 2,
-     },
-     {
-        question: "What team are known as ‘The Magpies?’",
-        option1: "Norwich City",
-        option2: "Brighton & Hove Albion F.C",
-        option3: "Watford F.C",
-        option4: "Newcastle United",
-        answer: 4,
-     },
+let questions = [{
+      question: "What team won the first ever Premier League season?",
+      option1: "Manchester United",
+      option2: "Aston Villa",
+      option3: "Liverpool",
+      option4: "Queens Park Ranger",
+      answer: 1,
+   },
+   {
+      question: "Name the Champions of the 2020/21 season?",
+      option1: "Manchester United",
+      option2: "Chelsea",
+      option3: "Manchester City",
+      option4: "Manchester United",
+      answer: 3,
+   },
+   {
+      question: "Which team went an entire season without losing a game in 2003/04?",
+      option1: "Manchester United",
+      option2: "Arsenal",
+      option3: "Liverpool",
+      option4: "Chelsea",
+      answer: 2,
+   },
+   {
+      question: "Which team won their first ever Premier League title in 2016?",
+      option1: "Tottenham Hotspur",
+      option2: "Leicester City",
+      option3: "Everton",
+      option4: "Southampton F.C",
+      answer: 2,
+   },
+   {
+      question: "What team are known as ‘The Magpies?’",
+      option1: "Norwich City",
+      option2: "Brighton & Hove Albion F.C",
+      option3: "Watford F.C",
+      option4: "Newcastle United",
+      answer: 4,
+   },
 ]
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
 function runGame() {
-    questionCounter = 0;
-    score = 0;
-    remainingQuestions = [...questions];
-    questions = questions.sort(() => Math.random() - 0.5);
-    console.log(remainingQuestions);
-    displayNextQuestion();
-    };
+   questionCounter = 0;
+   score = 0;
+   remainingQuestions = [...questions];
+   questions = questions.sort(() => Math.random() - 0.5);
+   console.log(remainingQuestions);
+   displayNextQuestion();
+};
 
 function displayNextQuestion() {
    question.innerHTML = questions[currentIndex].question;
    questionCounter++;
-   currentQuestion = remainingQuestions[questionIndex];
+   currentQuestion = questions[currentIndex];
 
+   /*display options*/
    options.forEach((option) => {
       const number = option.dataset.number;
+      console.log(currentQuestion)
       option.innerText = currentQuestion['option' + number];
-  });
-  };
-
-  displayNextQuestion();
-
-function displayOptions() {
-   
+   });
 };
 
-function checkAnswer() {
+displayNextQuestion();
 
-}
+function displayOptions() {
+
+};
+
+
+
 runGame()
 
-function checkAnswer() {
-
-}
 
 function calculateCorrectAnswer() {
 
